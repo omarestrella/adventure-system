12822be022d4f59bba90f70ba435732f75799855
Omar Estrella <omar.estrella@inin.com>
2017-08-31 17:12:33 -0400

    more renames

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject adventure-bot "0.1.0-SNAPSHOT"
+(defproject adventure-system "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
